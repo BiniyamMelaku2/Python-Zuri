4c2e2c4feacc42110e95386e208dc2f94ebf6474
Biniyam Melaku <biniyammelaku2@gmail.com>
2022-06-21 20:22:21 +0300

    print user details

---
+++ javascript-Introduction/mydetails.js
@@ -0,0 +1,9 @@
+// javascript code that prints out your name, height, and country on the screen
+
+let mydetails = function () 
+{
+	return "Name: Biniyam M. Endeshaw,\n" +
+           "Height: 170 cm,\n" +
+		   "Country: Ethiopia\n";
+};
+console.log(mydetails());
