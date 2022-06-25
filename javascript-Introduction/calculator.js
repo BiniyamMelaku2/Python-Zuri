f0328433205039b9a2c21c585c3cbf9e405f5597
Biniyam Melaku <biniyammelaku2@gmail.com>
2022-06-25 19:35:27 +0300

    basic arithmetic calculator without a frontend

--- javascript-Introduction/calculator.js
+++ javascript-Introduction/calculator.js
@@ -2,37 +2,49 @@
 
 
 
-const prompt = require('prompt-sync')();
+//const prompt = require('prompt-sync')();
 
 // take operator input
-const operator = prompt('Please type in the math operation you would like to complete:\n'+
-'+ for addition\n' + '- for subtraction\n' + '* for multiplication\n' + '/ for division\n' +
-'% for modulus\n ');
+const startCalculator = () => {
+	const operator = prompt('Please type in the math operation you would like to complete:\n'+
+	'+ for addition\n' + '- for subtraction\n' + '* for multiplication\n' + '/ for division\n' +
+	'% for modulus\n ');
 
-// take the operand input
-const number1 = parseFloat(prompt('Enter first number: '));
-const number2 = parseFloat(prompt('Enter second number: '));
+	// take the operand input
+	const number1 = parseFloat(prompt('Enter first number: '));
+	const number2 = parseFloat(prompt('Enter second number: '));
 
-let result;
+	let result;
 
-if (operator == '+') {
-    result = number1 + number2;
-}
-else if (operator == '-') {
-    result = number1 - number2;
-}
-else if (operator == '*') {
-    result = number1 * number2;
-}
-else if (operator == '/') {
-    result = number1 / number2;
-}
-else if (operator == '%') {
-    result = number1 % number2;
-}
-else {
-	console.log("You have not typed a valid operator, please run the program again.");
-	return "wrong operator !!";
+	if (operator == '+') {
+    		result = number1 + number2;
+	}
+	else if (operator == '-') {
+    		result = number1 - number2;
+	}
+	else if (operator == '*') {
+    		result = number1 * number2;
+	}
+	else if (operator == '/') {
+    		result = number1 / number2;
+	}
+	else if (operator == '%') {
+    		result = number1 % number2;
+	}
+	else {
+		console.log("You have not typed a valid operator, please run the program again.");
+                alert("You have not typed a valid operator, please run the program again.");
+		return "wrong operator !!";
+	}
+        alert(`${number1} ${operator} ${number2} = ${result}`);
+	console.log(`${number1} ${operator} ${number2} = ${result}`);
 }
 
-console.log(`${number1} ${operator} ${number2} = ${result}`);
+//Add event to button
+const button = document.querySelector('button');
+button.addEventListener('click',e =>{
+    startCalculator();
+});
+
+//call function
+startCalculator();
